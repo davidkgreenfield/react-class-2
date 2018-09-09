379184ac0f9ca9059eb99f918bd4d757b585f593
davidkgreenfield <davidkgreenfield@comcast.net>
2018-09-09 13:48:44 -0500

    added auth0

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Button } from 'react-bootstrap';
 import MyNewComponent from './components/MyNewComponent'
 
+
 class App extends Component {
+
+    login() {
+        this.props.auth.login();
+    }
+
+    logout() {
+        this.props.auth.logout();
+    }
+
+
   constructor(props){
     super(props)
     this.state = {"initValue":"hello there"}
   }
 
   render() {
+    const { isAuthenticated } = this.props.auth;
+    if(!isAuthenticated()){
+      return <div>
+          <Button
+              id="qsLoginBtn"
+              bsStyle="primary"
+              className="btn-margin"
+              onClick={this.login.bind(this)}
+          >
+              Log In
+          </Button>
+            </div>
+    }
+
     return (
       <div className="App">
+          <Button
+              id="qsLogoutBtn"
+              bsStyle="primary"
+              className="btn-margin"
+              onClick={this.logout.bind(this)}
+          >
+              Log Out
+          </Button>
         <MyNewComponent initValue={this.state.initValue}/>
       </div>
     );
---
+++ src/auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+    domain: 'cloudtraining.auth0.com',
+    clientId: '3bjqUSSic9GWq76YqiEmZXCjdZhMBu1j',
+    callbackUrl: 'http://localhost:3000/callback'
+}
